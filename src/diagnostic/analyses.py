c8f504a401187b00905205ab65ee15bfe92806d8
EduFalbel <du.falbel@gmail.com>
2023-02-24 16:57:38 +0100

    Add to_latex methods

--- src/diagnostic/analyses.py
+++ src/diagnostic/analyses.py
@@ -2,6 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 from enum import Enum, member
+from collections import defaultdict
 
 from abc import ABC, abstractmethod
 
@@ -11,6 +12,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 import matplotlib.pyplot as plt
+
+from pylatex import Document, Section, Subsection, Command
+from pylatex.base_classes import LatexObject
+
+from .latex_string import LatexString
 
 class Abbreviations(Enum):
     pass
@@ -41,6 +47,8 @@ class CountSummaryStatsOptions(Options):
 class Analysis(ABC):
 
     options: Options
+    section_title: str
+
 
     def __init__(self, comparison: pd.DataFrame, selection: list[str]=None) -> None:
         logging.info("%s", type(self))
@@ -58,6 +66,10 @@ class Analysis(ABC):
     @abstractmethod
     def generate_analysis(self, comparison) -> None:
         pass
+    
+    @abstractmethod
+    def to_latex(self) -> LatexObject:
+        pass
 
     def to_pdf(self) -> None:
         pass
@@ -65,14 +77,30 @@ class Analysis(ABC):
 class CountComparison(Analysis):
 
     options = CountComparisonOptions
+    section_title: str = "Link counts comparison analyses"
+
 
     def generate_analysis(self, comparison) -> None:
         for name in self.selection:
             comparison[name] = self.options[name].value(comparison)
+        self.comparison = comparison
+
+    def to_latex(self) -> LatexObject:
+        styler = self.comparison.style
+        return LatexString(
+            styler.to_latex(
+                caption="Link by link comparison of traffic counts",
+                position="H",
+                label="table:link_count"
+            ),
+            escape=True
+        )
+
 
 class CountSummaryStats(Analysis):
 
     options = CountSummaryStatsOptions
+    section_title: str = "Link counts summary statistics"
 
     # TODO: Allow user to specify mapping of columns to stats. For example:
     # mapping = {'count_obs': [min, max, mean], 'diff' : [RMS, MA]}
@@ -80,11 +108,24 @@ class CountSummaryStats(Analysis):
     # This can become an issue if the specified column was not calculated in CC
 
     def generate_analysis(self, comparison) -> None:
-        self.statistics = {}
+        statistics = defaultdict(list)
 
         for name in self.selection:
             logging.debug('%s', name)
-            self.statistics[name] = self.options[name].value(comparison)
+            statistics[name].append(self.options[name].value(comparison))
+        print(statistics)
+        self.statistics = pd.DataFrame.from_dict(statistics)
+
+    def to_latex(self) -> LatexObject:
+        styler = self.statistics.style
+        return LatexString(
+            styler.to_latex(
+                caption="Summary statistics for traffic counts",
+                position="H",
+                label="table:summary_stats"
+            ),
+            escape=True
+        )
 
 class CountVisualization(Analysis):
 
@@ -107,12 +148,24 @@ class Report():
     def __init__(self, title: str, simulated: pd.DataFrame, observed: pd.DataFrame, analyses: dict[Analysis, list[str]]) -> None:
         self.title = title
         self.comparison = self.create_comparison_df(simulated, observed)
-        self.analyses = [analysis(self.comparison, selection) for (analysis, selection) in analyses.items()]
+        print(self.comparison)
+        self.analyses: list[Analysis] = [analysis(self.comparison, selection) for (analysis, selection) in analyses.items()]
 
     def create_comparison_df(self, simulated: pd.DataFrame, observed: pd.DataFrame):
         return observed.merge(simulated, on='link_id', how='left', suffixes=['_obs', '_sim'])
 
-    def to_file(self, filename: str):
+    def fill_latex_doc(self, doc: Document):
+
         for analysis in self.analyses:
-            analysis.to_file(filename)
+            with doc.create(Section(analysis.section_title)):
+                doc.append(analysis.to_latex())
+
+    def to_latex(self, filepath: str):
+        doc = Document('basic')
+        self.fill_latex_doc(doc)
+        doc.generate_tex(filepath)
+
+    def to_file(self, filepath: str):
+        for analysis in self.analyses:
+            analysis.to_file(filepath)
         pass
