d56a839f8dfaa4db012a96e82c7653e930dd9958
EduFalbel <du.falbel@gmail.com>
2023-03-03 16:02:41 +0100

    Add method to escape latex characters

--- src/diagnostic/latex_string.py
+++ src/diagnostic/latex_string.py
@@ -3,12 +3,20 @@ from pylatex.utils import escape_latex
 
 class LatexString(LatexObject):
     """LatexObject subclass meant to hold a given latex string and return it when self.dumps() is called"""
-    def __init__(self, latex_string: str, escape: bool, *args, **kwargs):
-        # if escape: latex_string = escape_latex(latex_string)
-        # self.latex_string = latex_string
+    def __init__(self, latex_string: str, escape: list[str], *args, **kwargs):
+        if escape:
+            print(f"Escape chars: {escape}")
+            latex_string = self.escape(latex_string, escape)
+        self.latex_string = latex_string
         super().__init__(*args, **kwargs)
     def dumps(self):
         return self.latex_string
+
+    def escape(self, string: str, escape_chars: list[str]) -> str:
+        for char in escape_chars:
+            replacement = "\\" + char
+            string = string.replace(char, replacement)
+        return string
 
 class LatexStringTable(LatexString):
     def __init__(self, latex_string: str, escape: bool, *args, **kwargs):
