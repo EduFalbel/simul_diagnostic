6d163e53603b796d1e395d3dd8025687121d71a4
EduFalbel <du.falbel@gmail.com>
2023-03-03 16:40:48 +0100

    Create FigureContainer class

--- src/diagnostic/latex_string.py
+++ src/diagnostic/latex_string.py
@@ -1,5 +1,7 @@
-from pylatex.base_classes import LatexObject
+from pylatex.base_classes import LatexObject, Container
+from pylatex.utils import NoEscape
 from pylatex.package import Package
+from pylatex import Figure
 
 class LatexString(LatexObject):
     """LatexObject subclass meant to hold a given latex string and return it when self.dumps() is called"""
@@ -24,3 +26,13 @@ class LatexStringTable(LatexString):
     def __init__(self, latex_string: str, escape: bool, *args, **kwargs):
             super().__init__(latex_string, escape, *args, **kwargs)
 
+class FigureContainer(Container):
+    def __init__(self, paths, *, data=None, width=NoEscape(r'\textwidth'), position='H'):
+        data = [Figure(position=position) for path in paths]
+        for fig, path in zip(data, paths):
+            fig.add_image(str(path), width=width)
+        print(f"Data: {data}\nWidth: {width}")
+        super().__init__(data=data)
+
+    def dumps(self):
+        return self.dumps_content()
