Fix SQV calculation

src/diagnostic/analyses.py
@@ -27,7 +27,7 @@ class Options(Enum):
 class CountComparisonOptions(Options):
         DIFF = member(lambda comp: comp['count_sim'] - comp['count_obs'])
         PCT_DIFF = member(lambda comp: (comp['count_sim'] - comp['count_obs'])/comp['count_obs'])
-        SQV = member(lambda comp: 1/(1+np.sqrt((comp['count_sim'] - comp['count_obs'])**2/(comp['count_obs']*(comp['count_obs']**np.log10(comp['count_obs']))))))
+        SQV = member(lambda comp: 1/(1+np.sqrt((comp['count_sim'] - comp['count_obs'])**2/(comp['count_obs']*10**(comp['count_obs']//10)))))
         GEH = member(lambda comp: np.sqrt(2*(comp['count_sim'] - comp['count_obs'])**2/(comp['count_sim'] + comp['count_obs'])))
 
 class CountSummaryStatsOptions(Options):