f7f4320a82920eccb1bf6d71aaa3b1ecd2fe2584
EduFalbel <du.falbel@gmail.com>
2023-05-16 17:04:20 +0200

    Refactoring - extract code to functions

--- src/map_matching/match_detector_matsim.py
+++ src/map_matching/match_detector_matsim.py
@@ -11,22 +11,11 @@ import matsim
 def get_coordinates_geopy(directions: pd.DataFrame, col_name: str = 'Richtung', city: str = 'Zurich') -> gpd.GeoDataFrame:
     return gpd.GeoDataFrame(directions.assign(direction_coord = gpd.tools.geocode(directions[col_name] + ", " + city)['geometry']))
 
-def create_links_nodes(links: pd.DataFrame, nodes: pd.DataFrame) -> gpd.GeoDataFrame:
-
-    links_nodes = gpd.GeoDataFrame(
-        links\
-        .merge(nodes, left_on='to_node', right_on='node_id')\
-        .assign(node_coord = lambda x: shp.points(x['x'], x['y']))\
-        .drop(columns=['x', 'y'])
-    )
-
-    return links_nodes
-
-def closest_link(row, links_nodes: gpd.GeoDataFrame, net_name_col: str = "osm:way:name", detect_name_col: str = "Achse"):
+def closest_link(row, network: gpd.GeoDataFrame, net_name_col: str = "osm:way:name", detect_name_col: str = "Achse"):
     # print(f"Row: {row}")
 
     possible_links: gpd.GeoDataFrame = \
-        links_nodes[links_nodes[net_name_col] == row[detect_name_col]][['link_id', 'geometry', 'node_coord']]\
+        network[network[net_name_col] == row[detect_name_col]][['link_id', 'geometry', 'node_coord']]\
         .assign(distance = lambda x: x['geometry'].distance(row['geometry']))\
         .nsmallest(1, columns='distance', keep='all')
     if len(possible_links) == 1:
@@ -47,11 +36,44 @@ def closest_link(row, links_nodes: gpd.GeoDataFrame, net_name_col: str = "osm:wa
     row['link_id'] = possible_links.iloc[0]['link_id']
     return row
 
-def get_full_links(net: matsim.Network) -> gpd.GeoDataFrame:
+# Create detector dataframe with detector and direction geometries ##############
+def create_full_detectors(detectors, geocoded, direction_col):
+    detectors = detectors.merge(geocoded, on=direction_col).rename({"geometry_x": "geometry", "geometry_y": "direction_coord"}, axis=1)
+    detectors = detectors[~detectors["direction_coord"].isna()]
+
+    return detectors
+
+# Create network from MATSim output with link and node geometries as well as the custom link attributes ######################
+
+def create_full_network_from_matsim(network: matsim.Network, filter_link_col: str):
+    full_links = merge_links_with_attributes(network)
+
+    nodes = network.nodes
+
+    if filter_link_col:
+        full_links = full_links[full_links[filter_link_col] == True]
+        nodes = nodes[nodes['id'].isin(full_links['from_node']) | nodes['id'].isin(full_links['to_node'])]
+
+    return merge_links_with_nodes(full_links, nodes)
+
+def merge_links_with_attributes(net: matsim.Network) -> gpd.GeoDataFrame:
     links = net.as_geo().set_crs(epsg=2056)
     link_attrs = net.link_attrs.pivot(index='link_id', columns='name', values='value')
     assert set(links['link_id'].to_list()) == set(link_attrs.index.to_list())
     return links.merge(link_attrs, on='link_id')
+
+def merge_links_with_nodes(links: pd.DataFrame, nodes: pd.DataFrame) -> gpd.GeoDataFrame:
+
+    links_nodes = gpd.GeoDataFrame(
+        links\
+        .merge(nodes, left_on='to_node', right_on='node_id')\
+        .assign(node_coord = lambda x: shp.points(x['x'], x['y']))\
+        .drop(columns=['x', 'y'])
+    )
+
+    return links_nodes
+
+# Command-line interface #######################
 
 @click.command()
 @click.argument('detectors_filename', type=click.Path(exists=True))
@@ -79,28 +101,19 @@ def cli(detectors_filename, network_filename, direction_col, geocoded_directions
 
     geocoded = geocoded.to_crs(epsg=2056)
 
-    detectors = detectors.merge(geocoded, on=direction_col).rename({"geometry_x": "geometry", "geometry_y": "direction_coord"}, axis=1)
-    detectors = detectors[detectors["direction_coord"] != None]
+    detectors = create_full_detectors(detectors, geocoded, direction_col)
     print(detectors)
     
     network = matsim.read_network(network_filename)
-    nodes = network.nodes
-    full_links = get_full_links(network)
-    full_links.to_file("full_links.shp")
-    
-    if filter_link_col:
-        full_links = full_links[full_links[filter_link_col] == True]    
-        nodes = nodes[nodes['id'].isin(full_links['from_node']) | nodes['id'].isin(full_links['to_node'])]
+    full_network = create_full_network(network, filter_link_col)
 
-    links_nodes = create_links_nodes(full_links, nodes)
-
-    associated_detectors = gpd.GeoDataFrame(detectors.apply(closest_link, links_nodes=links_nodes, axis=1)).drop(columns=["direction_coord"])
+    associated_detectors = gpd.GeoDataFrame(detectors.apply(closest_link, network=full_network, axis=1)).drop(columns=["direction_coord"])
     # associated_detectors.to_file(output_filename)
 
-    associated_links = links_nodes.merge(associated_detectors, how='inner', on='link_id').set_geometry("geometry_x")
+    associated_links = full_network.merge(associated_detectors, how='inner', on='link_id').set_geometry("geometry_x")
     associated_nodes = associated_links.drop(columns=["geometry_y", "geometry_x"]).set_geometry("node_coord")
     print(associated_links.columns)
-    # associated_links.drop(columns=["geometry_y", "node_coord"]).to_file('link_with_network.shp')
+    associated_links.drop(columns=["geometry_y", "node_coord"]).to_file('link_with_network.shp')
     # associated_nodes.to_file('nodes.shp')
     
 if __name__ == '__main__':
